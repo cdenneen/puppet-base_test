1564d04bad1757dc054b29eb8842effed70334dd
Chris Denneen <cdenneen@gmail.com>
2017-03-27 11:51:49 -0400

    another attempt at merge

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,9 +2,9 @@ class base_test(
   $notify_message = 'base',
   $filebeat_prospectors = {}
 ) {
+  $filebeat_prospectors_final = hiera_hash('filebeat::prospectors', $filebeat_prospectors)
   class {'filebeat':
-    prospectors       => $filebeat_prospectors,
-    prospectors_merge => true
+    prospectors       => $filebeat_prospectors_final,
   }
   class {'notify_test':
     message => $notify_message
