df0514bb6419c223d6349c3cf95de74fcbd2e91e
Chris Denneen <cdenneen@gmail.com>
2017-03-27 12:05:30 -0400

    Using deep_merge from stdlib

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class base_test(
   $notify_message = 'base',
   $filebeat_prospectors = {}
 ) {
-  $filebeat_prospectors_final = hiera_hash('filebeat::prospectors', $filebeat_prospectors)
+  $filebeat_prospectors_final = deep_merge('filebeat::prospectors', $filebeat_prospectors)
   class {'filebeat':
     prospectors       => $filebeat_prospectors_final,
   }
